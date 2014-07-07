0a1e0ed95e79ac3bc2944add6934e34b28687e56
Jeremy Odoffin <Jeremy.Odoffin@gmail.com>
2014-07-07 00:59:19 +0000

    Associating with Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629113244) do
+ActiveRecord::Schema.define(version: 20140706145834) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
